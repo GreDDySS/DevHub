09b7368fc088f8b2e5b19cbff0ea63f974fb3fef
GreDDySS <misha.sudos@yandex.ru>
2025-12-15 00:58:29 +0800

    The user interface element is a project card

---
+++ DevHub.UI/Controls/ProjectCardControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DevHub.UI.Controls
+{
+    public partial class ProjectCardControl : UserControl
+    {
+        public ProjectCardControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
