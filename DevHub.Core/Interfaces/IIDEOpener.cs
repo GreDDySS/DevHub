9a26ba442f76aac25f40a5f3b82b46b23baa65e2
GreDDySS <misha.sudos@yandex.ru>
2025-12-15 00:53:16 +0800

    A service for opening an IDE for projects

---
+++ DevHub.Core/Interfaces/IIDEOpener.cs
@@ -0,0 +1,9 @@
+﻿using DevHub.Core.Models;
+
+namespace DevHub.Core.Interfaces
+{
+    public interface IIDEOpener
+    {
+        void OpenProject(string projectPath, IDEInfo ide);
+    }
+}
---
+++ DevHub.Core/Services/IDEOpener.cs
@@ -0,0 +1,32 @@
+﻿using DevHub.Core.Interfaces;
+using DevHub.Core.Models;
+using System.Diagnostics;
+
+namespace DevHub.Core.Services
+{
+    public class IDEOpener : IIDEOpener
+    {
+        public void OpenProject(string projectPath, IDEInfo ide)
+        {
+            string targetPath = projectPath;
+
+            if (ide.Name.Contains("Visual Studio", StringComparison.OrdinalIgnoreCase) || ide.ExecutablePath.Contains("dotenv.exe", StringComparison.OrdinalIgnoreCase))
+            {
+                var slnFiles = Directory.GetFiles(projectPath, "*.sln", SearchOption.TopDirectoryOnly);
+                if (slnFiles.Length > 0)
+                {
+                    targetPath = slnFiles[0];
+                }
+            }
+
+            var args = string.Format(ide.Arguments, targetPath);
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = ide.ExecutablePath,
+                Arguments = args,
+                UseShellExecute = true
+            });
+        }
+    }
+}
