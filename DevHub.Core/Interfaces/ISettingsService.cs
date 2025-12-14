6cbe50fab6703aa597441140293b31b7563f91d7
GreDDySS <misha.sudos@yandex.ru>
2025-12-15 00:55:39 +0800

    A service for setting up projects

---
+++ DevHub.Core/Interfaces/ISettingsService.cs
@@ -0,0 +1,10 @@
+﻿using DevHub.Core.Models;
+
+namespace DevHub.Core.Interfaces
+{
+    public interface ISettingsService
+    {
+        ProjectSettings LoadSettings();
+        void SaveSettings(ProjectSettings settings);
+    }
+}
