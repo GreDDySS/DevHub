f1cb4ed12b912c191e939cd7b532779b290b8b5b
GreDDySS <misha.sudos@yandex.ru>
2025-12-15 00:55:16 +0800

    A service for working with projects

---
+++ DevHub.Core/Interfaces/IProjectsService.cs
@@ -0,0 +1,11 @@
+﻿using DevHub.Core.Models;
+
+namespace DevHub.Core.Interfaces
+{
+    public interface IProjectsService
+    {
+        IReadOnlyList<ProjectInfo> Projects { get; }
+        IReadOnlyList<IDEInfo> IDEs { get; }
+        void Refresh();
+    }
+}
---
+++ DevHub.Core/Services/ProjectsService.cs
@@ -0,0 +1,27 @@
+﻿using DevHub.Core.Interfaces;
+using DevHub.Core.Models;
+
+namespace DevHub.Core.Services
+{
+    public class ProjectsService : IProjectsService
+    {
+        private readonly IProjectsScanner _scanner;
+        private readonly ProjectSettings _settings;
+
+        public List<ProjectInfo> ProjectsInternal { get; } = new();
+        public IReadOnlyList<ProjectInfo> Projects => ProjectsInternal;
+        public IReadOnlyList<IDEInfo> IDEs => _settings.IDEs;
+
+        public ProjectsService(IProjectsScanner scanner, ProjectSettings settings)
+        {
+            _scanner = scanner;
+            _settings = settings;
+        }
+
+        public void Refresh()
+        {
+            ProjectsInternal.Clear();
+            ProjectsInternal.AddRange(_scanner.Scan(_settings));
+        }
+    }
+}
