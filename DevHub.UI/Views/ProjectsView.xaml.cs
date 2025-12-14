3b7591c801f33886807ceb9a1c817d51c296b2c4
GreDDySS <misha.sudos@yandex.ru>
2025-12-15 01:01:38 +0800

    Project page

---
+++ DevHub.UI/Views/ProjectsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DevHub.UI.Views
+{
+    public partial class ProjectsView : UserControl
+    {
+        public ProjectsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
