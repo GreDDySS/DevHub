e40844bf10f7b1902bc4555d598d6542acf38895
GreDDySS <misha.sudos@yandex.ru>
2025-12-15 01:01:51 +0800

    ViewModels

---
+++ DevHub.UI/ViewModels/ProjectsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using DevHub.Core.Interfaces;
+using DevHub.Core.Models;
+using DevHub.UI.Base;
+using System.Collections.ObjectModel;
+using DevHub.UI.ViewModels;
+using DevHub.Core.Services;
+
+namespace DevHub.UI.ViewModels
+{
+    public class ProjectsViewModel : BaseViewModel
+    {
+        public ObservableCollection<ProjectCardViewModel> Projects { get; }
+        public ProjectSettings Settings { get; private set; }
+
+        public ProjectsViewModel(IProjectsService service, ProjectSettings settings)
+        {
+            Settings = settings;
+            service.Refresh();
+
+            var ideOpener = new IDEOpener();
+
+            Projects = new ObservableCollection<ProjectCardViewModel>(
+                service.Projects.Select(p => new ProjectCardViewModel(p, service.IDEs, ideOpener, settings)));
+        }
+
+        public void UpdateSettings(IProjectsService service, ProjectSettings newSettings)
+        {
+            Settings = newSettings;
+            service.Refresh();
+            Projects.Clear();
+            var ideOpener = new IDEOpener();
+            foreach (var p in service.Projects)
+            {
+                Projects.Add(new ProjectCardViewModel(p, service.IDEs, ideOpener, newSettings));
+            }
+        }
+    }
+}
