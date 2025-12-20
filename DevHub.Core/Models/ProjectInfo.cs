89e6454340db1679a75de2fc5e799a5382f49ff3
GreDDySS <misha.sudos@yandex.ru>
2025-12-20 23:45:49 +0800

    Rename LastModifided to LastModified in ProjectInfo

--- DevHub.Core/Models/ProjectInfo.cs
+++ DevHub.Core/Models/ProjectInfo.cs
@@ -6,6 +6,6 @@
         public string Description { get; set; } = "Нет описание...";
         public string Language { get; set; } = string.Empty;
         public string Path { get; set; } = string.Empty;
-        public DateTime LastModifided { get; set; }
+        public DateTime LastModified { get; set; }
     }
 }
