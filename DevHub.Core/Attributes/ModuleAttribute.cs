9bb55c22e2aa49a4f397d58a35b1d7c0f7b880b9
GreDDySS <misha.sudos@yandex.ru>
2025-12-21 01:22:03 +0800

    Enhance IModule and add ModuleAttribute for plugin architecture

---
+++ DevHub.Core/Attributes/ModuleAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace DevHub.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class ModuleAttribute : Attribute
+    {
+        public string Name { get; }
+        public bool IsSystem { get; }
+
+        public ModuleAttribute(string name, bool isSystem = false)
+        {
+            Name = name;
+            IsSystem = isSystem;
+        }
+    }
+}
--- DevHub.Core/Interfaces/IModule.cs
+++ DevHub.Core/Interfaces/IModule.cs
@@ -5,5 +5,7 @@ namespace DevHub.Core.Interfaces
     public interface IModule
     {
         void RegisterTypes(IServiceCollection services);
+
+        void OnInitialized(IServiceProvider provider);
     }
 }
